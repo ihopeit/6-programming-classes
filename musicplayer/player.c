e59505e610f335de39c9200f00d38c4ceec853eb
david ullua <davidullua@ihopeit.com>
2022-09-09 21:10:12 +0800

    comment for windows Sleep() func

--- musicplayer/player.c
+++ musicplayer/player.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h> // for usleep(), comment this line for Windows
+#include <unistd.h> // Windows: #include <windows.h>,  replace usleep() -> Sleep()
 #include <string.h> //strlen
 #include "bass.h"
 

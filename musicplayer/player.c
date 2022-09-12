04490a4229bc560c7e97c1fa667c023f7460d00d
david ullua <davidullua@ihopeit.com>
2022-09-12 17:12:00 +0800

    support multiple audio files

--- musicplayer/player.c
+++ musicplayer/player.c
@@ -12,32 +12,35 @@ BOOL startsWith(const char *pre, const char *str){
 int main(int argc, char** argv){
     BASS_Init(-1, 44100, 0, 0, NULL); 
     if(argc==1){
-        printf("Usage: ./play xxx.mp3 \n");
+        printf("Usage: ./play xxx.mp3 yyy.mp3 \n");
         return -1;
     }
     HSTREAM stream = 0;
-    if(startsWith("http", argv[1])){
-        stream = BASS_StreamCreateURL(argv[1], 0, 0, NULL, 0);
-    } else {
-        stream = BASS_StreamCreateFile(FALSE, argv[1], 0, 0, 0);
-    }
 
-    if(!stream){
-        printf("Usage: ./play xxx.mp3 \n");
-        printf("Failed to open file, %s code: %d\n", argv[1], BASS_ErrorGetCode());
-        return -2;
-    }
+    for(int i=1; i<=argc; i++){
+        if(startsWith("http", argv[i])){
+            stream = BASS_StreamCreateURL(argv[i], 0, 0, NULL, 0);
+        } else {
+            stream = BASS_StreamCreateFile(FALSE, argv[i], 0, 0, 0);
+        }
 
-    BOOL result = BASS_ChannelPlay(stream, TRUE);
-    if(!result){
-        printf("Failed to play stream, code: %d\n", BASS_ErrorGetCode());
-    }
-	
-    while (TRUE) {
-        usleep(100);
-        if(!BASS_ChannelIsActive(stream)){
-            BASS_StreamFree(stream);
-            break;
+        if(!stream){
+            printf("Usage: ./play xxx.mp3 \n");
+            printf("Failed to open file, %s code: %d\n", argv[1], BASS_ErrorGetCode());
+            return -2;
+        }
+
+        BOOL result = BASS_ChannelPlay(stream, TRUE);
+        if(!result){
+            printf("Failed to play stream, code: %d\n", BASS_ErrorGetCode());
+        }
+            
+        while (TRUE) {
+            usleep(100);
+            if(!BASS_ChannelIsActive(stream)){
+                BASS_StreamFree(stream);
+                break;
+            }
         }
     }
 	

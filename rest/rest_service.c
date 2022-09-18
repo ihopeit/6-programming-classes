d457a5d87fb882eb6f0721b5f15c300f64d82f8a
david ullua <davidullua@ihopeit.com>
2022-09-18 22:10:43 +0800

    restful service framework

--- rest/rest_service.c
+++ rest/rest_service.c
@@ -9,60 +9,21 @@
 
 #include "rest_service.h"
 
-#define PORT 6875
-#define PREFIX "/multiple"
+#define PORT 8000
 
 #define DEFAULT_REALM  "default_realm"
 
 /**
  * callback functions declaration
  */
-int callback_multiple_level_zero (const struct _u_request * request, struct _u_response * response, void * user_data);
+int callback_city_history (const struct _u_request * request, struct _u_response * response, void * user_data);
 
-int callback_multiple_level_one (const struct _u_request * request, struct _u_response * response, void * user_data);
-
-int callback_multiple_level_two (const struct _u_request * request, struct _u_response * response, void * user_data);
-
-int callback_multiple_level_three (const struct _u_request * request, struct _u_response * response, void * user_data);
-
-int callback_multiple_level_one_complete (const struct _u_request * request, struct _u_response * response, void * user_data);
+int callback_month_ranking (const struct _u_request * request, struct _u_response * response, void * user_data);
 
 int callback_multiple_level_final (const struct _u_request * request, struct _u_response * response, void * user_data);
 
 int callback_default (const struct _u_request * request, struct _u_response * response, void * user_data);
 
-/**
- * decode a u_map into a string
- */
-char * print_map(const struct _u_map * map) {
-  char * line, * to_return = NULL;
-  const char **keys, * value;
-  int len, i;
-  if (map != NULL) {
-    keys = u_map_enum_keys(map);
-    for (i=0; keys[i] != NULL; i++) {
-      value = u_map_get(map, keys[i]);
-      len = snprintf(NULL, 0, "key is %s, value is %s", keys[i], value);
-      line = o_malloc((len+1)*sizeof(char));
-      snprintf(line, (len+1), "key is %s, value is %s", keys[i], value);
-      if (to_return != NULL) {
-        len = o_strlen(to_return) + o_strlen(line) + 1;
-        to_return = o_realloc(to_return, (len+1)*sizeof(char));
-        if (o_strlen(to_return) > 0) {
-          strcat(to_return, "\n");
-        }
-      } else {
-        to_return = o_malloc((o_strlen(line) + 1)*sizeof(char));
-        to_return[0] = 0;
-      }
-      strcat(to_return, line);
-      o_free(line);
-    }
-    return to_return;
-  } else {
-    return NULL;
-  }
-}
 
 int main (int argc, char **argv) {
   int ret;
@@ -70,7 +31,7 @@ int main (int argc, char **argv) {
   // Set the framework port number
   struct _u_instance instance;
   
-  y_init_logs("multiple_callbacks_example", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting multiple_callbacks_example");
+  y_init_logs("pm25_rest_service", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting pm25 service");
   
   if (ulfius_init_instance(&instance, PORT, NULL, DEFAULT_REALM) != U_OK) {
     y_log_message(Y_LOG_LEVEL_ERROR, "Error ulfius_init_instance, abort");
@@ -83,18 +44,13 @@ int main (int argc, char **argv) {
   instance.max_post_body_size = 1024;
   
   // Endpoint list declaration
-  ulfius_add_endpoint_by_val(&instance, "*", PREFIX, "/zero/*", 99, &callback_multiple_level_final, NULL);
+  ulfius_add_endpoint_by_val(&instance, "GET", "pm25", "/history/*", 0, &callback_city_history, NULL);
+  ulfius_add_endpoint_by_val(&instance, "GET", "pm25", "/month/*", 0, &callback_month_ranking, NULL);
+
+  // callback final to release memories assigned:
+  ulfius_add_endpoint_by_val(&instance, "*", "pm25", "/*", 99, &callback_multiple_level_final, NULL);
   
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/zero/*", 0, &callback_multiple_level_zero, NULL);
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/zero/one/*", 1, &callback_multiple_level_one, NULL);
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/zero/one/two/*", 2, &callback_multiple_level_two, NULL);
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/zero/one/two/three/", 3, &callback_multiple_level_three, NULL);
-  
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/zero/onec/*", 1, &callback_multiple_level_one_complete, NULL);
-  ulfius_add_endpoint_by_val(&instance, "GET", PREFIX, "/zero/onec/two/*", 2, &callback_multiple_level_two, NULL);
-  
-  
-  // default_endpoint declaration
+  // default_endpoint declaration, return 404 for other URLs
   ulfius_set_default_endpoint(&instance, &callback_default, NULL);
   
   // Start the framework
@@ -118,75 +74,24 @@ int main (int argc, char **argv) {
   return 0;
 }
 
-/**
- * Callback function of level zero
- */
-int callback_multiple_level_zero (const struct _u_request * request, struct _u_response * response, void * user_data) {
-  ulfius_set_string_body_response(response, 200, "Level zero");
-  response->shared_data = o_strdup("shared from level zero");
+int callback_city_history (const struct _u_request * request, struct _u_response * response, void * user_data){
+  char * new_body;
+  new_body = msprintf("url: %s history data:", request->http_url);
+  ulfius_set_string_body_response(response, 200, new_body);
+  o_free(new_body);
+  y_log_message(Y_LOG_LEVEL_DEBUG, "request url: %s", response->shared_data);
   return U_CALLBACK_CONTINUE;
 }
 
-/**
- * Callback function of level one
- */
-int callback_multiple_level_one (const struct _u_request * request, struct _u_response * response, void * user_data) {
-  char old_body[response->binary_body_length + 1], * new_body;
-  
-  memcpy(old_body, response->binary_body, response->binary_body_length);
-  old_body[response->binary_body_length] = '\0';
-  new_body = msprintf("%s\n%s", old_body, "Level one");
+int callback_month_ranking (const struct _u_request * request, struct _u_response * response, void * user_data){
+  char * new_body;
+  new_body = msprintf("url: %s ranking by month:", request->http_url);
   ulfius_set_string_body_response(response, 200, new_body);
   o_free(new_body);
-  y_log_message(Y_LOG_LEVEL_DEBUG, "shared_data is %s", response->shared_data);
+  y_log_message(Y_LOG_LEVEL_DEBUG, "request url: %s", response->shared_data);
   return U_CALLBACK_CONTINUE;
 }
 
-/**
- * Callback function of level one with complete return
- */
-int callback_multiple_level_one_complete (const struct _u_request * request, struct _u_response * response, void * user_data) {
-  char old_body[response->binary_body_length + 1], * new_body;
-  
-  memcpy(old_body, response->binary_body, response->binary_body_length);
-  old_body[response->binary_body_length] = '\0';
-  new_body = msprintf("%s\n%s", old_body, "Level one");
-  ulfius_set_string_body_response(response, 200, new_body);
-  o_free(new_body);
-  y_log_message(Y_LOG_LEVEL_DEBUG, "shared_data is %s", response->shared_data);
-  o_free(response->shared_data);
-  return U_CALLBACK_COMPLETE;
-}
-
-/**
- * Callback function of level two
- */
-int callback_multiple_level_two (const struct _u_request * request, struct _u_response * response, void * user_data) {
-  char old_body[response->binary_body_length + 1], * new_body;
-  
-  memcpy(old_body, response->binary_body, response->binary_body_length);
-  old_body[response->binary_body_length] = '\0';
-  new_body = msprintf("%s\n%s", old_body, "Level two");
-  ulfius_set_string_body_response(response, 200, new_body);
-  o_free(new_body);
-  y_log_message(Y_LOG_LEVEL_DEBUG, "shared_data is %s", response->shared_data);
-  return U_CALLBACK_CONTINUE;
-}
-
-/**
- * Callback function of level three
- */
-int callback_multiple_level_three (const struct _u_request * request, struct _u_response * response, void * user_data) {
-  char old_body[response->binary_body_length + 1], * new_body;
-  
-  memcpy(old_body, response->binary_body, response->binary_body_length);
-  old_body[response->binary_body_length] = '\0';
-  new_body = msprintf("%s\n%s", old_body, "Level three");
-  ulfius_set_string_body_response(response, 200, new_body);
-  o_free(new_body);
-  y_log_message(Y_LOG_LEVEL_DEBUG, "shared_data is %s", response->shared_data);
-  return U_CALLBACK_CONTINUE;
-}
 
 /**
  * Final callback function called on multiple calls
